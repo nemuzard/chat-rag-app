fb87ee1e0d1adb42f60e5bc0f85a73c038900109
nemuzard <nemuzard@gmail.com>
2025-11-14 22:30:04 -0600

    added db initialization

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,23 +1,23 @@
 package main
 
+
 import(
 	"log"
 	"net/http"
-	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin" 
 )
 
 func main(){
-
+	// create a gin router
 	r:=gin.Default()
+	// register a get route -/health
 	r.GET("/health",func(c *gin.Context){
 		c.JSON(http.StatusOK,gin.H{
 
 			"status":"ok",
 		})
-	}
-
-	)
-
+	})
+	// start on port 8080
 	if err:=r.Run(":8000"); err!=nil{
 
 		log.Fatalf("server failed to start: %v",err)
---
+++ backend/internal/store/db.go
@@ -0,0 +1,47 @@
+package store
+import(
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+// initialize db connection
+func initDB(){
+	// localhost for now
+	host:="localhost"
+	port:=5432
+	user:="chat_app"
+	password:="chat_app"
+	dbName:="chat_app"
+
+	dsn:=fmt.Sprintf(
+		"host=%s user=%s password=%s dbName=%s port=%d sslmode=disable TimeZone=UTC",
+		host,user,password,dbName,port,
+	)
+
+	// gorm logger 
+	newLogger:=logger.New(
+		log.New(os.Stdout,"\r\n",log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel: logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful: true,
+		},
+	)
+	db,err := gorm.Open(postgres.Open(dsn),&gorm,Config{
+		Logger:=newLogger,
+	})
+	if err!=nil{
+		log.Fatalf("failed to connect database: %v",err)
+
+	}
+	DB = db
+}
